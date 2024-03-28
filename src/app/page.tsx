8603609a37d60789b7bd15e55f5d3a6839a1c45a
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-03-28 01:10:46 -0300

    Add Home page background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Profile from '@/components/Profile'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col'>
+    <main className='flex min-h-screen flex-col bg-[#DFE2F4]'>
       <Profile username='KingHaate' level={999} stars={4} />
     </main>
   )
