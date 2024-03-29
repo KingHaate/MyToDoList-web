d506132bbcef25b63055dc9d1ed2eb09ebfc44a1
KingHaate_ <samuelsamagaia13@gmail.com>
2024-03-28 23:27:59 -0300

    Add h3 typography component

---
+++ src/components/ui/typography/h3.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils"
+import { DetailedHTMLProps, HTMLAttributes } from "react"
+
+export function H3({
+  children,
+  className,
+  ...props
+}: DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) {
+  return (
+    <h3
+      className={cn(
+        "scroll-m-20 text-2xl font-semibold tracking-tight", className
+      )}
+      {...props}
+    >
+      { children }
+    </h3>
+  )
+}
